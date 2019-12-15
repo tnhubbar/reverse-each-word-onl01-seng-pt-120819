5e6b867d18b91b11902c3d87cd1c500ff0d329a0
Taryn Johnson <taryn.johnson919@gmail.com>
2019-12-15 17:27:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,6 +6,6 @@ def reverse_each_word(string)
  complete_array = []
   array_string = string.split 
   array_string.each do |word|
-    complete_array << word.reverse
+   word.reverse.push 
   end 
 end 
