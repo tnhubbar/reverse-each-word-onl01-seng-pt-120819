87f7e308ceece350390f324205a28b1de2440a9b
Taryn Johnson <taryn.johnson919@gmail.com>
2019-12-15 17:46:08 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,12 @@
+# Write a method called reverse_each_word that takes in a string argument of a sentence and returns that same sentence with each word reversed in place.
+
+# First solve it using .each Then utilize the same method using .collect to see the difference.
+
+def reverse_each_word(string)
+ complete_array = []
+  array_string = string.split 
+  array_string.collect do |word|
+   word.reverse!
+end 
+ return array_string.join(" ")
+end 
